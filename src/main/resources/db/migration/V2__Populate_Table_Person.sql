0f181083e48027a9c8e6fa0a629677f2e0fa1c1a
Gustavo Moraes <gustavo.moliveira@bandtec.com.br>
2022-08-11 10:19:52 -0300

    added migrations with flyway plugin

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`address`, `first_name`, `gender`, `last_name`) VALUES
+    ('Sao Paulo', 'Ayrton', 'Male', 'Senna'),
+    ('Campinas', 'Carlos', 'Male', 'Henrique'),
+    ('Belo Horizone', 'Maria', 'Female', 'Julia'),
+    ('Sao Paulo', 'Ana', 'Female', 'Alice'),
+    ('Bahia', 'Joao', 'Male', 'Alves'),
+    ('Rio de Janeiro', 'Gabriela', 'Female', 'Fonseca');
