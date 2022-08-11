37baaac7847268369f65c40132d056aea5dc9dde
Gustavo Moraes <gustavo.moliveira@bandtec.com.br>
2022-08-11 11:16:55 -0300

    added support for yaml and xml on header param

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -3,8 +3,8 @@ package br.com.erudio.controllers;
 import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.services.PersonServices;
+import br.com.erudio.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,35 +16,59 @@ public class PersonController {
     @Autowired
     private PersonServices personServices;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<br.com.erudio.data.vo.v1.PersonVO> findAll() {
+    @GetMapping(produces = {MediaType.APPLICATION_JSON,
+                            MediaType.APPLICATION_XML,
+                            MediaType.APPLICATION_YML
+    })
+    public List<PersonVO> findAll() {
         return personServices.findAll();
     }
 
     @RequestMapping(value = "/{id}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML
+    })
     public PersonVO findById(@PathVariable(value = "id") Long  id) {
         return personServices.findById(id);
     }
 
     @PostMapping(
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML
+            },
+            consumes = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML})
     public PersonVO create(@RequestBody PersonVO person) {
         return personServices.create(person);
     }
 
     @PostMapping(
             value = "/v2",
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML
+            },
+            consumes = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML
+            }
+    )
     public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
         return personServices.createV2(person);
     }
 
     @PutMapping(
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML
+            },
+            consumes = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML
+            })
     public PersonVO update(@RequestBody PersonVO person) {
         return personServices.update(person);
     }
